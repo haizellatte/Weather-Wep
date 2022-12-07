68eb9db7d96236157d168a6fa06cbea010bd90b7
haizel <shyoeun5000@gmail.com>
2022-12-07 15:20:21 +0900

    기본세팅완료_ver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [location, setLocation] = useState('');
+  const [result, setResult] = useState({});
+  const API_key = `7b529bbd02138dd0ed83445de338a6d5`; 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_key}`
+  const today = new Date().toLocaleDateString()
+  let day = new Date();
+  const weekDay = ['SUN', 'MON','TUE', 'WED','THU','FRI', 'SAT'];
+  let Week = weekDay[day.getDay()];
+
+
+  //!event handler
+  const onChangeLocation = (e) => {
+    setLocation(e.target.value);
+  }
+  //인풋창에 도시를 입력하고 엔터 -> Weather API 호출(비동기적으로)
+  const searchWeather = async(e) => {
+    if(e.key === 'Enter'){
+      try {
+        const data = await axios({
+          //weather API의 날씨정보를 가져오는 것임으로 GET 메서드 사용
+          method: 'get',
+          url: url
+        })
+        console.log(data);
+        setResult(data);
+      }
+      catch (err){
+        alert('Please check the city name again');
+      }
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='day'>{today} {Week}</div>
+      <h2>Today's weather</h2>
+      <input 
+      type = 'text'
+      placeholder='        Which City'
+      value={location}
+      onChange={onChangeLocation}
+      onKeyDown={searchWeather}
+      />
+      {Object.keys(result).length !== 0 && (
+        <div className='answer_form'>
+        <div className='sky'>
+          {result.data.weather[0].description}
+        </div>
+        <div className='temperature'>
+          { Math.round(((result.data.main.temp - 273.15) * 10)) / 10}°c
+        </div>
+        <div className='temperature_max_min'>
+          <span className='_min'>
+             { Math.round(((result.data.main.temp_min - 273.15) * 10)) / 10}°
+          </span>
+          <span className='v-line' />
+          <span className='_max'>
+             { Math.round(((result.data.main.temp_max - 273.15) * 10)) / 10}°
+          </span>
+        </div>
+        <span className='line'/>
+        <div className='city'>{result.data.name}<span className='country'>. {result.data.sys.country}</span></div>
+      </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+/*result.data.main.temp온도는 켈빈단위로 -> 섭씨온도로 바꿔줘야함
+켈빈단위 -> 섭씨 변환식 : (온도 - 273.15)
+*/
---
+++ src/imfomation_test.js
@@ -0,0 +1,49 @@
+// 20221205112017
+// https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=7b529bbd02138dd0ed83445de338a6d5
+
+{
+    "coord": {
+      "lon": 126.9778,
+      "lat": 37.5683
+    },
+    "weather": [
+      {
+        "id": 804,
+        "main": "Clouds",
+        "description": "overcast clouds",
+        "icon": "04d"
+      }
+    ],
+    "base": "stations",
+    "main": {
+      "temp": 270.81,
+      "feels_like": 267.4,
+      "temp_min": 269.84,
+      "temp_max": 270.81,
+      "pressure": 1031,
+      "humidity": 18,
+      "sea_level": 1031,
+      "grnd_level": 1024
+    },
+    "visibility": 10000,
+    "wind": {
+      "speed": 2.46,
+      "deg": 318,
+      "gust": 4.32
+    },
+    "clouds": {
+      "all": 89
+    },
+    "dt": 1670206443,
+    "sys": {
+      "type": 1,
+      "id": 5509,
+      "country": "KR",
+      "sunrise": 1670193092,
+      "sunset": 1670228029
+    },
+    "timezone": 32400,
+    "id": 1835848,
+    "name": "Seoul",
+    "cod": 200
+  }
